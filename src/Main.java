d0d30ed2ef974876b6ce6ae773f547f5d31fdf32
al-rosenthal <alfred.k.rosenthal@gmail.com>
2022-07-06 13:41:25 -0700

    making a proper graph

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,8 @@ public class Main {
 //                }
 //            }
 
-            System.out.println("OG: \n" + d.printData(d.map));
-            System.out.println("Reverse: \n" + d.printData(d.reverse));
+//            System.out.println("OG: \n" + d.printData(d.map));
+//            System.out.println("Reverse: \n" + d.printData(d.reverse));
 
             d.orderMap(d.findEnd());
 
@@ -44,7 +44,7 @@ public class Main {
 
             // assume all lines in file will have only 2 items
             if (pairs.length == 2) {
-                data.add(new Relation(pairs[0], pairs[1]));
+//                data.add(new Relation(pairs[0], pairs[1]));
             }
         }
         scanner.close();
@@ -55,92 +55,34 @@ public class Main {
 
 
 class Relation {
-    String start, end;
-    Relation(String start, String end) {
-        this.start = start;
-        this.end = end;
-    }
-}
-class DataOrganizer {
-    HashMap<String, List<Relation>> map = new HashMap<>();
-    HashMap<String, List<Relation>> reverse = new HashMap<>();
-
-    DataOrganizer(List<Relation> relations) {
-        for(Relation r: relations) {
-            // add first item
-            // add relation to first item
-            // add second item if not exists
-            if (!map.containsKey(r.start)) {
-                map.put(r.start, new ArrayList<>());
-            }
-            if (!map.containsKey(r.end)) {
-                map.put(r.end, new ArrayList<>());
-            }
-
-            if (!reverse.containsKey(r.start)) {
-                reverse.put(r.start, new ArrayList<>());
-            }
-            if (!reverse.containsKey(r.end)) {
-                reverse.put(r.end, new ArrayList<>());
-            }
-
-            map.get(r.start).add(r);
-            reverse.get(r.end).add(r);
-        }
-    }
-
-    public List<String> findEnd() {
-        List<String> end = new ArrayList<>();
-
-        for(String key: map.keySet()) {
-            List<Relation> item = map.get(key);
-            if (item.isEmpty()) {
-                // found an end piece
-                end.add(key);
-            }
-        }
-        return end;
-    }
-
-    public void orderMap(List<String> node) {
-        System.out.println(node);
-        // probably would be better for work with these as relations
-        // map them as a list of relations
-        // then we can check what the parent is without having to scan the map?
-        /*
-            !empty
-            for each item, look for them in the reverse
-            check for relations up?
-
-
-         */
-        if(!node.isEmpty()) {
-            for (String key: node) {
-                List<String> items = new ArrayList<>();
-                for (Relation r: reverse.get(key)) {
-                    items.add(r.start);
-                }
-                orderMap(items);
-            }
-        } else {
-            // I guess we are all done
-        }
-
-    }
-
-    public String printData(HashMap<String, List<Relation>> data) {
-        StringBuilder b = new StringBuilder();
-        for (String key: data.keySet()) {
-            b.append(key + ": ");
-            for (Relation item: data.get(key)) {
-                b.append(item.end + ", ");
-            }
-            b.append("\n");
-        }
-        return b.toString();
+    String value;
+    List<String> next;
+    Relation(String start) {
+        this.value = start;
+        this.next = new ArrayList<>();
     }
 }
 
+
+class DataGraph {
+    List<Relation> nodes = new ArrayList<>();
+
+    public void add(Relation r) {
+        nodes.add(r);
+    }
+
+    public Relation find(String key) {
+        Relation found = null;
+
+        for(Relation r: nodes) {
+            if(r.value == key) {
+                found = r;
+            }
+        }
+
+        return found;
+    }
+}
 
 /*
 
